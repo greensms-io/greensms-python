d6ca4a6e1da5c1aa0c8e61d620b21987f862d8f3
Manan Jadhav <manan.jadhav@gmail.com>
2020-11-04 21:20:07 +0530

    Fixed Error class

---
+++ greensms/http/error.py
@@ -0,0 +1,37 @@
+from builtins import super
+
+class RestError(Exception):
+
+  def __init__(self, error):
+
+    error_message = ''
+    if type(error) == Exception:
+      error_message = error.message
+    else:
+      error_message = error['error']
+      self.name = error['name'] if 'name' in error.keys() else 'RestError'
+      self.code = error['code'] if 'code' in error.keys() else -1
+
+    self.error = error_message
+    self.message = error_message
+
+    super().__init__(self.message)
+
+    error_type = self._get_error_type(self.code)
+    self.error_type = error_type
+
+    if 'params' in error.keys():
+      self.params = error['params']
+
+
+  def _get_error_type(self, code):
+    if code == 0:
+      return 'AUTH_DECLINED'
+    elif code == 1:
+      return 'MISSING_INPUT_PARAM'
+    elif code == 2:
+      return 'INVALID_INPUT_PARAM'
+    elif code == 404:
+      return 'NOT_FOUND'
+    else:
+      return 'INTERNAL_SERVER_ERROR'
--- greensms/http/rest.py
+++ greensms/http/rest.py
@@ -1,8 +1,13 @@
 import requests
+from error import RestError
 
 class HttpClient:
   def __init__(self, opts):
     print('Hello World')
+
+    if 1 != 2:
+      raise RestError({ 'error': 'Hello Error'})
+
 
   def request(self, **kwargs):
     print('Request')
