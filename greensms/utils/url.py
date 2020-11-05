27db5ed96d767a17b26227d74398c2cc9f945696
Manan Jadhav <manan.jadhav@gmail.com>
2020-11-05 21:18:10 +0530

    added url helper

---
+++ greensms/utils/url.py
@@ -0,0 +1,18 @@
+try:
+  from urllib.parse import urlparse, urljoin
+except ImportError:
+  from urlparse import urlparse, urljoin
+from functools import reduce
+from greensms.constants import BASE_URL
+
+def base_url():
+  return BASE_URL
+
+def build_url(base_url, *args):
+  if not base_url:
+    raise Exception('Base URL cannot be empty!')
+
+  path = '/'.join(args)
+
+  url = urljoin(base_url, path)
+  return url
