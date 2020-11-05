5b546d4dc6b105d38519c3f61c111e29780968ae
Manan Jadhav <manan.jadhav@gmail.com>
2020-11-06 00:27:55 +0530

    Added validator

---
+++ greensms/utils/validation.py
@@ -0,0 +1,23 @@
+from greensms.http.error import RestError
+from cerberus import Validator
+
+
+def validate(schema, data):
+  error_result = None
+  validator = Validator(schema)
+
+  if validator.validate(data):
+    return error_result
+  else:
+    error_result = RestError({
+      'code': 1,
+      'error': 'Validation Error',
+      'params': validator.errors
+    })
+
+  return error_result
+
+
+
+
+
