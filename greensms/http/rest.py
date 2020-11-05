3727ae92cbc258c8bfe88ff25fa0689f17c89ba9
Manan Jadhav <manan.jadhav@gmail.com>
2020-11-05 21:17:58 +0530

    fixed http rest client

--- greensms/http/rest.py
+++ greensms/http/rest.py
@@ -14,8 +14,45 @@ class HttpClient:
 
     if 'method' not in kwargs:
       raise Exception('Http Method is required')
+    method = kwargs['method']
 
-    if 'uri' not in kwargs:
-      raise Exception('URI is required')
+    if 'url' not in kwargs:
+      raise Exception('URL is required')
+    url = kwargs['url']
 
     headers = kwargs['headers'] if 'headers' in kwargs else {}
+
+    if self.token:
+      headers['Authorization'] = 'Bearer ' + self.token
+
+    del kwargs['method']
+    del kwargs['url']
+    if 'headers' in kwargs:
+      del kwargs['headers']
+
+    params = {}
+    if bool(self.default_params):
+      for key, value in self.default_params.items():
+        params[key] = value
+
+    if 'params' in kwargs:
+      for key, value in kwargs['params']:
+        params[key] = value
+
+    data = {}
+    if bool(self.default_data):
+      for key, value in self.default_data.items():
+        data[key] = value
+
+    if 'data' in kwargs:
+      for key, value in kwargs['data']:
+        data[key] = value
+
+    response = requests.request(method=method, url=url, headers=headers, params=params)
+
+    response = response.json()
+
+    if response['error']:
+      response = RestError(response)
+
+    return result
