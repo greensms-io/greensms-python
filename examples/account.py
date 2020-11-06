9f64e8ffc9a213c4bccef42c88cd967d0dcc821c
Manan Jadhav <manan.jadhav@gmail.com>
2020-11-06 05:38:08 +0530

    Fixed account examples

--- examples/account.py
+++ examples/account.py
@@ -8,7 +8,13 @@ def get_token():
   res = client.account.token(expire=100)
   print(res)
 
+def get_tariff():
+  res = client.account.tariff()
+  print(res)
+
 if __name__ == '__main__':
   get_balance()
 
   get_token()
+
+  get_tariff()
