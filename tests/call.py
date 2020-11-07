0a6fde9935f4789a484389b8ffc87d9c4cb4256b
Manan Jadhav <manan.jadhav@gmail.com>
2020-11-07 09:54:03 +0530

    Added tests for call

---
+++ tests/call.py
@@ -0,0 +1,20 @@
+import unittest
+from tests.default import client
+from tests.utils import random_phone
+
+class TestCallMethods(unittest.TestCase):
+
+  def test_send(self):
+    response = client.call.send(to=random_phone())
+    self.assertIn('request_id', response)
+
+  def test_mandatory_to(self):
+    self.assertRaises(Exception, client.call.send())
+
+  def test_status(self):
+    response = client.call.status(id='1fd4ac4d-6e4f-4e32-b6e4-8087d3466f55', extended=True)
+    self.assertIn('status', response)
+
+
+if __name__ == '__main__':
+  unittest.main()
