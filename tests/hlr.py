edaa6eee6c551a2f1f5536a92ec28c4c41c2130b
Manan Jadhav <manan.jadhav@gmail.com>
2020-11-07 10:13:12 +0530

    Fixed params for hlr tests

--- tests/hlr.py
+++ tests/hlr.py
@@ -5,14 +5,14 @@ from tests.utils import random_phone
 class TestHlrMethods(unittest.TestCase):
 
   def test_send(self):
-    response = client.hlr.send(to=random_phone())
+    response = client.hlr.send(to=random_phone(79260000111, 79260999999))
     self.assertIn('request_id', response)
 
   def test_mandatory_to(self):
     self.assertRaises(Exception, client.hlr.send())
 
   def test_status(self):
-    response = client.hlr.status(id='70d296f5-ac52-403d-a27b-24829c2faebc',)
+    response = client.hlr.status(id='70d296f5-ac52-403d-a27b-24829c2faebc', to=random_phone())
     self.assertIn('status', response)
 
 
