323c43d0a362480dee2ca4025ebc402f8a852455
Manan Jadhav <manan.jadhav@gmail.com>
2020-11-12 11:25:21 +0530

    Added test for social

---
+++ tests/social_test.py
@@ -0,0 +1,29 @@
+import unittest
+from tests.default import client
+from tests.utils import random_phone
+
+
+class TestSocialMethods(unittest.TestCase):
+
+    def test_send(self):
+        dict_params = {
+            'to': random_phone(),
+            'txt': 'Test Message Hampshire',
+            'from': 'PyTest',
+            'tag': 'PyTest',
+            'hidden': 'Hampshire'
+        }
+
+        response = client.social.send(**dict_params)
+        self.assertIn('request_id', response)
+
+    def test_mandatory_to(self):
+        self.assertRaises(Exception, client.social.send())
+
+    def test_status(self):
+        response = client.social.status(id='caf3efb1-8aca-4387-9ed0-e667d315c5c9')
+        self.assertIn('status', response)
+
+
+if __name__ == '__main__':
+    unittest.main()
