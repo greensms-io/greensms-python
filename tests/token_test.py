c926ccb8fa7010628a1d8a72545cdb801d890673
Manan Jadhav <manan.jadhav@gmail.com>
2020-11-19 23:35:40 +0530

    Removed token hardcoding

--- tests/token_test.py
+++ tests/token_test.py
@@ -3,13 +3,11 @@ import time
 from tests.default import client
 from greensms.client import GreenSMS
 
-token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoidGVzdCIsImlhdCI6MTYwNTc5NjEwOCwiaXNzIjoiYXBpLmdyZWVuc21zLnJ1In0.St8-5fJqQnHx1MFybJ5o4D5VZ-RK3HxcL0DScJsOYec'  # noqa: E501
-
-
 class TestTokenMethods(unittest.TestCase):
 
     def test_balance(self):
-        token_client = GreenSMS(token=token)
+        token_response = client.account.token(expire=5)
+        token_client = GreenSMS(token=token_response.access_token)
         response = token_client.account.balance()
         self.assertIn('balance', response)
 
