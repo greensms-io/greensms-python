d31cfa022b84b38a2951820104ce74f849f0b1bb
Manan Jadhav <manan.jadhav@gmail.com>
2023-03-06 02:05:31 +0530

    fix: remove typo

--- greensms/utils/cascade_validator.py
+++ greensms/utils/cascade_validator.py
@@ -2,7 +2,7 @@ def cascade_validator(field, value, error):
     if not value or len(value) == 0:
         return
 
-    split_arr = input_str.split(",")
+    split_arr = value.split(",")
 
     for cascade_str in split_arr:
         if cascade_str.lower() not in ['viber', 'sms', 'voice']:
